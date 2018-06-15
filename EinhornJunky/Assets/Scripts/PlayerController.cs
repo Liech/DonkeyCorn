1ba905363f7f1e53703b913c2be7b18419e91a57
Niki <Niki@2e4c87aa-350a-41d1-a3ce-48a50c81b2a6>
2018-06-15 21:00:30 +0000

    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/SummerGameJam18@9 2e4c87aa-350a-41d1-a3ce-48a50c81b2a6

--- EinhornJunky/Assets/Scripts/PlayerController.cs
+++ EinhornJunky/Assets/Scripts/PlayerController.cs
@@ -16,20 +16,26 @@ public class PlayerController : MonoBehaviour {
     EinhornPhysic p = gameObject.GetComponent<EinhornPhysic>();
     Vector2 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
 
-    if (p.IsGrounded){
       if (Input.GetKey(KeyCode.A))
       {
         p.VX = -WalkSpeed;
-      }
-      else if (Input.GetKey(KeyCode.D))
+        transform.GetChild(0).GetComponent<Animator>().Flip = false;
+    }
+    else if (Input.GetKey(KeyCode.D))
       {
         p.VX = WalkSpeed;
+        transform.GetChild(0).GetComponent<Animator>().Flip = true;
       }
       else p.VX = 0;
 
+    if (p.IsGrounded)
+    {
       if (Input.GetKey(KeyCode.Space))
         p.VY = JumpForce;
     }
+
+    //if (p.VY <= 0)
+      //p.VY += -1f;
 
     Camera.main.transform.position = Camera.main.transform.position + new Vector3(transform.position.x- Camera.main.transform.position.x, 0,0);
   }
