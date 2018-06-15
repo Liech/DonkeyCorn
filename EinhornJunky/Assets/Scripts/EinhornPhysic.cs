b21639fbbe7d506f9c9bc7cd697b903dfd1e8eb7
Niki <Niki@2e4c87aa-350a-41d1-a3ce-48a50c81b2a6>
2018-06-15 20:08:20 +0000

    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/SummerGameJam18@7 2e4c87aa-350a-41d1-a3ce-48a50c81b2a6

---
+++ EinhornJunky/Assets/Scripts/EinhornPhysic.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EinhornPhysic : MonoBehaviour {
+  //public float Gravity = 1;
+  //public float JumpDuration = 0.3f;
+  //public float JumpForce = 0;
+
+  public Vector2 Velocity; 
+
+  public Rigidbody2D body
+  {
+    get
+    {
+      return gameObject.GetComponent<Rigidbody2D>();
+    }
+  }
+
+  public float VX
+  {
+    get
+    {
+      return body.velocity.x;
+    }
+    set
+    {
+      body.velocity = new Vector2(value, body.velocity.y);
+    }
+  }
+  public float VY
+  {
+    get
+    {
+      return body.velocity.y;
+    }
+    set
+    {
+      body.velocity = new Vector2(body.velocity.x, value);
+    }
+  }
+
+  public bool IsGrounded
+  {
+    get
+    {
+      Vector3 position = transform.position;
+      position.y = GetComponent<Collider2D>().bounds.min.y - 0.1f;
+      float length = 0.1f - 0.1f;
+      Debug.DrawRay(position, Vector3.down * length);
+      bool grounded = Physics2D.Raycast(position, Vector3.down, length, ~0);
+      return grounded;
+    }
+  }
+
+  //public void Jump()
+  //{
+  //  Velocity = new Vector2(0,-JumpForce);
+
+  //}
+
+  // Use this for initialization
+  void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//if (!IsGrounded)
+  //  {
+  //    Velocity = Velocity - new Vector2(0,Gravity);
+  //  }
+
+  //  Vector2 velTodo = Velocity;
+  //  float stepsize = 0.1f;
+  //  while (velTodo.magnitude > 0)
+  //  {
+  //    transform.position = transform.position + new Vector3(0.1f, 0.1f, 0);
+  //    //velTodo-= 
+    //}
+
+  }
+}
--- EinhornJunky/Assets/Scripts/PlayerController.cs
+++ EinhornJunky/Assets/Scripts/PlayerController.cs
@@ -3,48 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-
-  public float JumpForce = 50;
-  public float WalkingSpeed = 5;
-
-  private float distanceToGround;
+  public float WalkSpeed = 1;
+  public float JumpForce = 5;
 
 	// Use this for initialization
 	void Start () {
-    distanceToGround = GetComponent<BoxCollider2D>().bounds.extents.y;
+    
 	}
-
-  public float isGroundedRayLength = 0.1f;
-  //public LayerMask layerMaskForGrounded;
-  public bool isGrounded
-  {
-    get
-    {
-      Vector3 position = transform.position;
-      position.y = GetComponent<Collider2D>().bounds.min.y - 0.1f;
-      float length = isGroundedRayLength - 0.1f;
-      Debug.DrawRay(position, Vector3.down * length);
-      bool grounded = Physics2D.Raycast(position, Vector3.down, length, ~0);
-      return grounded;
-    }
-  }
-
-  public void Jump()
-  {
-    Rigidbody2D body = gameObject.GetComponent<Rigidbody2D>();
-    body.velocity = new Vector2(gameObject.GetComponent<Rigidbody2D>().velocity.x, 0);
-    body.AddForce(new Vector2(0,JumpForce));
-  }
-
+   
   // Update is called once per frame
-  void FixedUpdate () {
-    if (isGrounded)
-      Jump();
+  void Update () {
+    EinhornPhysic p = gameObject.GetComponent<EinhornPhysic>();
+    Vector2 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
 
-    var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-    Rigidbody2D body = gameObject.GetComponent<Rigidbody2D>();
-    if (isGrounded)
-      body.AddForce(new Vector2(move.x * WalkingSpeed, 0));
+    if (p.IsGrounded){
+      if (Input.GetKey(KeyCode.A))
+      {
+        p.VX = -WalkSpeed;
+      }
+      else if (Input.GetKey(KeyCode.D))
+      {
+        p.VX = WalkSpeed;
+      }
+      else p.VX = 0;
 
+      if (Input.GetKey(KeyCode.Space))
+        p.VY = JumpForce;
+    }
+
+    Camera.main.transform.position = Camera.main.transform.position + new Vector3(transform.position.x- Camera.main.transform.position.x, 0,0);
   }
 }
