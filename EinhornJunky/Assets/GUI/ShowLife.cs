43b1486d67aad82b8539114554e31929c4434389
Niki <Niki@2e4c87aa-350a-41d1-a3ce-48a50c81b2a6>
2018-06-15 17:46:00 +0000

    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/SummerGameJam18@4 2e4c87aa-350a-41d1-a3ce-48a50c81b2a6

---
+++ EinhornJunky/Assets/GUI/ShowLife.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class ShowLife : MonoBehaviour {
+
+    public float MaxLife = 100;
+    public float CurrentDamage = 0;
+    public List<Sprite> Lifes;
+    public float YOffset = 3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    CurrentDamage++;
+    CurrentDamage = CurrentDamage % MaxLife;
+    int Index = (int)(10.0f * ((float)CurrentDamage / (float)MaxLife));
+    gameObject.GetComponent<Image>().sprite = Lifes[Index];
+	}
+}
