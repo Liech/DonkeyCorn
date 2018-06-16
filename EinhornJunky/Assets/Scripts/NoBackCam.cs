28c0fa3b4b6f02d1382cf5b241555fd9054d978d
Niki <Niki@2e4c87aa-350a-41d1-a3ce-48a50c81b2a6>
2018-06-16 16:14:14 +0000

    cam can go up
    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/SummerGameJam18@46 2e4c87aa-350a-41d1-a3ce-48a50c81b2a6

--- EinhornJunky/Assets/Scripts/NoBackCam.cs
+++ EinhornJunky/Assets/Scripts/NoBackCam.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class NoBackCam : MonoBehaviour {
 
   public bool CanGoBack = false;
+  public float yCamStart;
 
+  public void Start()
+  {
+    yCamStart = Camera.main.transform.position.y;
+  }
 
   public void Update()
   {
     GameObject p = GameObject.Find("Player");
     if (p == null) return;
-    Vector3 pos = Camera.main.transform.position + new Vector3(p.transform.position.x - Camera.main.transform.position.x, 0, 0);
+    Vector3 pos = Camera.main.transform.position + new Vector3(p.transform.position.x - Camera.main.transform.position.x, p.transform.position.y, 0);
     if (pos.x < Camera.main.transform.position.x && !CanGoBack)
       pos.x = Camera.main.transform.position.x;
-
+    if (pos.y < yCamStart)
+      pos.y = yCamStart;
     Camera.main.transform.position = pos;
   }
 }
