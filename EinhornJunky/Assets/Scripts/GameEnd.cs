f497db697c14f611edb931dfc3926e1f90fec27d
Niki <Niki@2e4c87aa-350a-41d1-a3ce-48a50c81b2a6>
2018-06-16 18:06:19 +0000

    Game End added
    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/SummerGameJam18@62 2e4c87aa-350a-41d1-a3ce-48a50c81b2a6

---
+++ EinhornJunky/Assets/Scripts/GameEnd.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnd : MonoBehaviour {
+
+  bool won = false;
+
+  // Update is called once per frame
+  void Update () {
+
+    if (!won)
+    {
+      GameObject Player = GameObject.Find("Player");
+      if (Player == null) return;
+      if (Player.transform.position.x > transform.position.x) won = true;
+    }
+
+    if (won) Debug.Log("Sieg");
+  }
+}
