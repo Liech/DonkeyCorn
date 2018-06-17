a50595c2a32aa6666c930097a45ef1bc8a6016ce
Niki <Niki@2e4c87aa-350a-41d1-a3ce-48a50c81b2a6>
2018-06-17 11:19:21 +0000

    candy corn gravity
    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/SummerGameJam18@138 2e4c87aa-350a-41d1-a3ce-48a50c81b2a6

--- EinhornJunky/Assets/Scripts/Corn.cs
+++ EinhornJunky/Assets/Scripts/Corn.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Corn : MonoBehaviour {
   public bool CanDrop = true;
 
-  public float Gravity;
+  public float Gravity = 1;
 
 	// Use this for initialization
 	void Start () {
@@ -26,7 +26,7 @@ public class Corn : MonoBehaviour {
         && Player.transform.position.x > col.bounds.min.x && !released)
       {
         gameObject.AddComponent<Rigidbody2D>();
-        //gameObject.GetComponent<Rigidbody2D>().
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = Gravity;
         released = true;
       }
     }
