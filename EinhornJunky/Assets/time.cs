3c4753b664216babad9942481b93889e13ec9756
Niki <Niki@2e4c87aa-350a-41d1-a3ce-48a50c81b2a6>
2018-06-17 01:20:23 +0000

    Time counter added
    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/SummerGameJam18@100 2e4c87aa-350a-41d1-a3ce-48a50c81b2a6

---
+++ EinhornJunky/Assets/time.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class time : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if (GameObject.Find("Canvas/WinScreen").GetComponent<Image>().enabled) return;
+    if (GameObject.Find("Canvas/LooseScreen").GetComponent<Image>().enabled) return;
+    float t = Time.time;
+    int minutes = (int)(t / 60.0f);
+    t -= minutes * 60;
+    int seconds = (int)(t);
+    t -= seconds;
+    int thenthseconds = (int)(t * 100);
+
+
+    GetComponent<Text>().text = minutes.ToString() + ":" + seconds + ":" + thenthseconds;
+	}
+}
