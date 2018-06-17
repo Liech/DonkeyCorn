c2ce3eca8bedbf236d5d13e41d2da9d10d41b655
Niki <Niki@2e4c87aa-350a-41d1-a3ce-48a50c81b2a6>
2018-06-17 10:40:41 +0000

    commit fix
    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/SummerGameJam18@132 2e4c87aa-350a-41d1-a3ce-48a50c81b2a6

--- EinhornJunky/Assets/Scripts/MusicMixer.cs
+++ EinhornJunky/Assets/Scripts/MusicMixer.cs
@@ -13,13 +13,14 @@ public class MusicMixer : MonoBehaviour {
 	void Update () {
     float lvl = GameObject.Find("Canvas/SugarLevel").GetComponent<SugarLevel>().CurrentLevel;
     AudioSource s = GetComponent<AudioSource>();
-
+    s.pitch = 0.5f + (lvl / 3.0f);
     if (lvl < FadeInStart)  s.volume = 0;
     else if (lvl > FadeOutEnd)  s.volume = 0;
     else if (lvl < FullVolume)
     {
       float perc = (lvl - FadeInStart) / (FullVolume - FadeInStart);
       s.volume = perc;
+      
     }
     else
     {
