2140026452055b3509f0b8cc1ec431c6791442a9
Niki <Niki@2e4c87aa-350a-41d1-a3ce-48a50c81b2a6>
2018-06-17 00:57:42 +0000

    Music added
    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/SummerGameJam18@98 2e4c87aa-350a-41d1-a3ce-48a50c81b2a6

---
+++ EinhornJunky/Assets/Scripts/MusicMixer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicMixer : MonoBehaviour {
+  public float FadeInStart;
+  public float FullVolume;
+  public float FadeOutEnd;
+  public bool FullL;
+  public bool FullR;
+
+	// Update is called once per frame
+	void Update () {
+    float lvl = GameObject.Find("Canvas/SugarLevel").GetComponent<SugarLevel>().CurrentLevel;
+    AudioSource s = GetComponent<AudioSource>();
+
+    if (lvl < FadeInStart)  s.volume = 0;
+    else if (lvl > FadeOutEnd)  s.volume = 0;
+    else if (lvl < FullVolume)
+    {
+      float perc = (lvl - FadeInStart) / (FullVolume - FadeInStart);
+      s.volume = perc;
+    }
+    else
+    {
+      float perc = (lvl - FadeInStart) / (FullVolume - FadeInStart);
+      s.volume = 1-perc;
+    }
+
+    if (FullL && lvl < FullVolume) s.volume = 1;
+    if (FullR && lvl > FullVolume) s.volume = 1;
+
+  }
+}
