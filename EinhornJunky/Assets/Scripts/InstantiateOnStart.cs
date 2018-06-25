9bc5ea74454060fc13679f9609517b1b82ac1219
Niki <Niki@2e4c87aa-350a-41d1-a3ce-48a50c81b2a6>
2018-06-25 21:03:07 +0000

    Canvas is now initialized automatticaly
    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/SummerGameJam18@199 2e4c87aa-350a-41d1-a3ce-48a50c81b2a6

---
+++ EinhornJunky/Assets/Scripts/InstantiateOnStart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateOnStart : MonoBehaviour {
+  public GameObject obj;
+  bool init = false;
+	// Use this for initialization
+	void Awake () {
+    if (init) return;
+    init = true;
+    GameObject O = Instantiate(obj);
+    O.name = obj.name;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
