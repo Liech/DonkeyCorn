d4e539c0ab732fd5089a0162ad414ffd14cbc3f9
Niki <Niki@2e4c87aa-350a-41d1-a3ce-48a50c81b2a6>
2018-12-09 00:50:07 +0000

    Replaced custom tilemap with unity tilemap
    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/SummerGameJam18@218 2e4c87aa-350a-41d1-a3ce-48a50c81b2a6

---
+++ EinhornJunky/Assets/Scripts/SugaredTilemap.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SugaredTilemap : MonoBehaviour {
+
+  public List<TileBase> DepriTiles;
+  public List<TileBase> SugarTiles;
+
+  private void Start()
+  {
+    old = SugarStatus.Overdrive;
+  }
+
+  SugarStatus old;
+	// Update is called once per frame
+	void Update () {  
+    SugarStatus s = GetComponent<SugarLevelDependent>().CurrentLevel;
+    if (old == s)
+      return;
+    old = s;
+    if (s == SugarStatus.Depri)
+    {
+      for (int i = 0; i < DepriTiles.Count; i++)
+        GetComponent<Tilemap>().SwapTile(SugarTiles[i], DepriTiles[i]);      
+    }
+    if (s == SugarStatus.Wach)
+    {
+      for (int i = 0; i < DepriTiles.Count; i++)
+        GetComponent<Tilemap>().SwapTile(DepriTiles[i], SugarTiles[i]);
+    }
+    if (s == SugarStatus.Overdrive)
+    {
+      for (int i = 0; i < DepriTiles.Count; i++)
+        GetComponent<Tilemap>().SwapTile(DepriTiles[i], SugarTiles[i]);
+    }
+  }
+}
